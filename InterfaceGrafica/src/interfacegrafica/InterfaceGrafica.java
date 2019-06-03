257b4baf809c9b4ec197a3987d9b45fd5d46440f
camilly <millyfernanda223@gmail.com>
2019-06-03 11:12:14 -0300

    primeira versão

---
+++ InterfaceGrafica/src/interfacegrafica/InterfaceGrafica.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interfacegrafica;
+
+/**
+ *
+ * @author aluno
+ */
+public class InterfaceGrafica {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //Conclusão concluir = new Conclusão();
+        //concluir.setVisible(true);
+        
+        TelaInicial tela = new TelaInicial();
+        tela.setVisible(true);
+        
+        
+        
+    }
+    
+}
